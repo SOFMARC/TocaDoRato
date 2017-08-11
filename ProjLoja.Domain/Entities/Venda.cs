ae321fb405e7dcb1a0adc0c61769dc33e09de5d6
sofmarc <sofmarc.silva@gmail.com>
2017-08-11 19:31:37 -0300

    entitirys

---
+++ ProjLoja.Domain/Entities/Venda.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjLoja.Domain.Entities
+{
+    public class Venda
+    {
+        public int VendaId { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public decimal Desconto { get; set; }
+
+        public decimal ValorTotal { get; set; }
+
+        public int Quantidade { get; set; }
+
+        public virtual IEnumerable<VendaItem> VendaItens { get; set; }
+
+        public int UsuarioId { get; set; }
+        public virtual Usuario Usuario { get; set; }
+    }
+}
---
+++ ProjLoja.Domain/Entities/VendaItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjLoja.Domain.Entities
+{
+    public class VendaItem
+    {
+        public int VendaItemId { get; set; }
+
+        public int FormaPagamento { get; set; }
+
+        public int StatusPagamento { get; set; }
+
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public int CondicaoPagamento { get; set; }
+
+        public int ProdutoId { get; set; }
+        public Produto Produto { get; set; }
+
+        public int ServicoId { get; set; }
+        public Servico Servico { get; set; }
+
+        public int VendaId { get; set; }
+        public Venda Venda { get; set; }
+    }
+}
